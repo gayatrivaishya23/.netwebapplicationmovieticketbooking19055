bad5c60774f079a255d2f6e2128396fcf381a8c1
gayatrivaishya23 <gayatrivaishya123@gmail.com>
2025-11-04 14:11:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using MovieEventBooking.Services;
+using MovieEventBooking.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
+// MongoDB Config
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+builder.Services.AddSingleton<MongoService>();
+builder.Services.AddSingleton<MongoService>();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Welcome}/{id?}");
+
+app.Run();
